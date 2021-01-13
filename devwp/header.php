2d19383b8ce8f33b4d3ddb0c13bae9ccc7068cd9
Myzwer <josh@onefortyfivedesign.com>
2021-01-13 11:04:54 -0500

    Got a working version of the navbar for mobile rolling. It is still static, so it will need to be adjusted later, as well as a desktop version.

--- devwp/header.php
+++ devwp/header.php
@@ -19,64 +19,17 @@
 <?php wp_head(); ?>
 </head>
 
-<div class="topnav" id="myTopnav">
-    <div class = "logo">
-        <a href="http://charisinaction.org/">
-            <img src = "http://starter-template.local/wp-content/uploads/2020/03/Logo.png" alt = "logo">
-        </a>
-    </div>
 
-    <div class = "pushypushy">
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-        <a href="javascript:void(0);" style="font-size:25px;" class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
+<?php //wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+
+
+
+<div class="sticky header-mobile" id="stickyBottom">
+    <ul>
+        <li><a class = "current-menu-item container" href="#"><i class="fas fa-home"></i></a></li>
+        <li><a href="#"><i class="fas fa-play-circle"></i></a></li>
+    </ul>
 </div>
-
-
-
-
-<!--<div class="topnav" id="myTopnav">
-    <div class = "logo">
-        <a href="http://charisinaction.org/">
-            <img src = "http://charisinaction.org/wp-content/uploads/2019/09/logo.png" alt = "logo">
-        </a>
-    </div>
-
-    <div class = "pushypushy">
-        <a class = "navpush" href="http://charisinaction.org/about">About Us</a>
-        <a href="http://charisinaction.org/partners">Partners</a>
-        <a href="http://charisinaction.org/gallery">Gallery</a>
-        <a href="http://charisinaction.org/blog">Blog</a>
-        <a href="http://charisinaction.org/contact">Contact</a>
-        <a href="https://www.paypal.com/us/webapps/mpp/donations">Donate</a>
-        <a href="javascript:void(0);" style="font-size:25px;" class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
-</div>-->
-
-
-
-
-
-<!--<div class = "pushypushy">
-    <a class = "navpush" href="http://charisinaction.org/about">About Us</a>
-    <div class="dropdown">
-        <button class="dropbtn">Get Involved
-            <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-            <a href="#">Special Projects</a>
-            <a href="#">Serve With Us</a>
-            <a href="#">Take a Trip</a>
-            <a href="#">Give Now</a>
-        </div>
-    </div>
-    <a href="http://charisinaction.org/partners">Partners</a>
-    <a href="http://charisinaction.org/gallery">Gallery</a>
-    <a href="http://charisinaction.org/blog">Blog</a>
-    <a href="http://charisinaction.org/contact">Contact</a>
-    <a href="https://www.paypal.com/us/webapps/mpp/donations">Donate</a>
-    <a href="javascript:void(0);" style="font-size:25px;" class="icon" onclick="myFunction()">&#9776;</a>
-</div>-->
 
 
 
