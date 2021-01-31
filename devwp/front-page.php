3da4ad80fe8a457f2cd332cc21ec13bf88d340d9
Myzwer <josh@onefortyfivedesign.com>
2021-01-31 17:19:48 -0500

    :heavy_plus_sign: Added the scss for the slanted sections, added a test section on the frontpage as well.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -21,7 +21,6 @@ get_header(); ?>
             <div class="grid-x grid-padding-x">
                 <div class="large-12 cell">
                     <h1>Welcome to 6!</h1>
-
                     <button class="btn btn-v1 center">Button 4</button>
                     <div class="callout">
                         <h3>We're stoked you want to try Foundation! <i class="fas fa-address-book"></i></h3>
@@ -108,8 +107,32 @@ get_header(); ?>
             </div>
         </div>
     </div>
-</div>
 
+
+    <div class="colored-slants">
+        <div class="slanted hunger">
+            <h1>Hunger</h1>
+            <p>According to Second Harvest Food Bank of East TN, 1 in 5 children and 1 in 8 adults are at risk for
+                hunger in East Tennessee. This is a total of 160,000 people among us who are food vulnerable. Learn how
+                you can make an impact in the lives of hungry people in our community and beyond.</p>
+            <div class="giveNow">
+                <a href="https://foothillschurch.com/hunger/" class="eventButton" style="width: 270px;"><span>Feed The Hungry </span></a>
+            </div>
+        </div>
+        <div class="slanted gre-2" style="background-color: red;">
+            <h1>Thirst</h1>
+            <p>Across the world there are millions of people who are affected by unclean drinking water every single
+                day. Hundreds of children die or are orphaned daily because the only water they have access to is dirty
+                water. This is entirely preventable. There are thousands of people in America who are affected by
+                natural disasters every year and need clean drinking water. Learn how you can support those who are
+                thirsty in the US and around the world.</p>
+            <div class="giveNow">
+                <a href="https://foothillschurch.com/thirst/" class="eventButton" style="width: 290px;"><span>Provide Clean Water </span></a>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 
 
