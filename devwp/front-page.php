1043ad4be539122590a515caddfcdabfb4ea7e51
Myzwer <josh@onefortyfivedesign.com>
2021-02-01 10:27:07 -0500

    :hammer: Fixed an issue where the slant caused background to show behind the slant but under the image. Basically I removed a colored strip of background that wasn't suppposed to be seen.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -23,37 +23,22 @@ get_header(); ?>
                     <h1 class = "dark-color-invert large-title">The Busy Pastor</h1>
                     <h3 class = "dark-color-invert">Be busy with the right things.</h3>
                     <p class = "dark-color-invert">Nam viverra risus sed erat cursus, at euismod ante lacinia. Pellentesque mauris mi, vehicula eget enim a, viverra bibendum tortor. Curabitur lacus dolor, feugiat in dolor quis, ultricies condimentum ex. Vestibulum semper elit tortor, et aliquet metus pulvinar dignissim. Nulla varius mi id libero accumsan vehicula. Integer nec elementum est.</p>
-                    <button class="btn btn-v1">Button 4</button>
+                    <button class="btn btn-v1"><span class="icon-icons8-play"></span> Watch Now</button>
                 </div>
             </div>
         </div>
     </div>
 
 
-
-
-    <div class="colored-slants">
-        <div class="slanted hunger">
-            <h1>Hunger</h1>
-            <p>According to Second Harvest Food Bank of East TN, 1 in 5 children and 1 in 8 adults are at risk for
-                hunger in East Tennessee. This is a total of 160,000 people among us who are food vulnerable. Learn how
-                you can make an impact in the lives of hungry people in our community and beyond.</p>
-            <div class="giveNow">
-                <a href="https://foothillschurch.com/hunger/" class="eventButton" style="width: 270px;"><span>Feed The Hungry </span></a>
-            </div>
-        </div>
-        <div class="slanted gre-2" style="background-color: red;">
-            <h1>Thirst</h1>
-            <p>Across the world there are millions of people who are affected by unclean drinking water every single
-                day. Hundreds of children die or are orphaned daily because the only water they have access to is dirty
-                water. This is entirely preventable. There are thousands of people in America who are affected by
-                natural disasters every year and need clean drinking water. Learn how you can support those who are
-                thirsty in the US and around the world.</p>
-            <div class="giveNow">
-                <a href="https://foothillschurch.com/thirst/" class="eventButton" style="width: 290px;"><span>Provide Clean Water </span></a>
-            </div>
-        </div>
+    <div class="slanted slant-light margin-pull-large">
+        <h2>The Busy Pastors</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consequat metus id nulla rutrum, nec consequat
+            arcu porta. Aenean egestas mattis mauris, quis fermentum nulla condimentum ac. Duis vel risus placerat,
+            ullamcorper mi et, varius justo. Integer eu erat ut nibh mollis tempus sed at velit. Sed ac lectus mattis,
+            suscipit est vel, laoreet elit. Curabitur venenatis pretium venenatis. Ut eget volutpat magna, eget ultrices
+            mi.</p>
     </div>
+
 
 </div>
 
