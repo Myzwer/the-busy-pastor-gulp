e940a6db71886aa5d7ce9ec05e08d7afce9d4370
Myzwer <josh@onefortyfivedesign.com>
2021-02-03 12:20:15 -0500

    :rocket: Added next / previous post functionality to the single posts page.

--- devwp/single.php
+++ devwp/single.php
@@ -12,7 +12,7 @@
 get_header(); ?>
 
     <div class="content">
-        <div class="grid-container inverted-background header-height full-width">
+        <div class="grid-container inverted-background header-height-single full-width">
             <div class="grid-x padding-top">
                 <div class="small-12 medium-6 cell">
                     <h2><?php the_title(); ?></h2>
@@ -24,7 +24,7 @@ get_header(); ?>
                     </div>
                 </div>
 
-                <div class="small-12  cell padding-top">
+                <div class="small-12 medium-11 cell padding-top">
                     <h3>Episode <?php the_field("episode_number"); ?></h3>
                     <p>Nam viverra risus sed erat cursus, at euismod ante
                         lacinia. Pellentesque mauris mi, vehicula eget enim a, viverra bibendum
@@ -34,8 +34,29 @@ get_header(); ?>
                 </div>
             </div>
         </div>
-    </div>
 
+
+        <div class="slanted slant-dark margin-pull-large">
+            <div class="grid-x grid-margin-x grid-padding-x">
+                <div class="small-12 cell">
+                    <h2 class="left dark-color-invert">More Episodes</h2>
+                </div>
+
+                <div class="small-12 medium-4 cell">
+                    <a href="<?php echo get_permalink( get_adjacent_post( false, '', false ) ); ?>"><button class="btn btn-v2-light"> Next Post</button></a>
+                </div>
+
+                <div class="small-12 medium-4 cell">
+                    <a href="/play"><button class="btn btn-v2-light"> All Episodes</button></a>
+                </div>
+
+                <div class="small-12 medium-4 cell">
+                    <a href="<?php echo get_permalink( get_adjacent_post( false, '', true ) ); ?>"><button class="btn btn-v2-light"> Previous Episodes</button></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 <?php
 get_footer();
