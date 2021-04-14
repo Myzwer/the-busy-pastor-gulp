126f06b5a0a6973fab3d0f263b9d989bfe991c83
Myzwer <josh@onefortyfivedesign.com>
2021-04-14 14:43:33 -0400

    :rocket: Added social icons to the footer.

--- devwp/footer.php
+++ devwp/footer.php
@@ -19,6 +19,9 @@
 
 <!-- FOOTER START -->
 <div class="footer">
+    <a href="https://www.facebook.com/busypastor"><i class="fab fa-facebook-square"></i></a>
+    <a href="https://www.instagram.com/busypastor/?hl=en"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.youtube.com/channel/UCwljG51omo4mJoJBhBAaQuw"><i class="fab fa-youtube"></i></a>
     <p>&#169; <?php echo date("Y"); ?> Foothills Church</p>
     <p><a href="/privacy-policy">Privacy Policy</a></p>
 </div>
